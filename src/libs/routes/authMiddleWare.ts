433d66a57a96bf8a1735d7a31a3385fc5fdcbb45
pvvarun <varun.mishra@successive.tech>
2019-02-04 19:07:58 +0530

    featur: updated authmiddleware.ts (#40012)

--- src/libs/routes/authMiddleWare.ts
+++ src/libs/routes/authMiddleWare.ts
@@ -9,22 +9,25 @@ export default function authMiddleWare( module:string , permissionType:string ) 
   const token = req.headers.token;
   console.log(token);
   const decoded = jwt.verify(token, key);
-  const { role } = decoded;
+  console.log(decoded);
+  const { _id } = decoded;
+  const id = String(_id);
+  console.log( id );
   const errMessage = (`Unauthorized access`);
   const userRepo = new UserRepository();
-  if(!role) {
-        throw({ error : errMessage, statusCode:403});
-  }
-  console.log(decoded);
+  // if(!role) {
+  //       throw({ error : errMessage, statusCode:403});
+  // }
+  // console.log(decoded);
 
-  const result = permission(module,role,permissionType);
+  //const result = permission(module,role,permissionType);
 
-  if(!result) {
-    throw ({ error : errMessage, statusCode:403});
-  }
-  else {
-    next();
-  }
+  // if(!result) {
+  //   throw ({ error : errMessage, statusCode:403});
+  // }
+  // else {
+  //   next();
+  // }
 
 
   }
